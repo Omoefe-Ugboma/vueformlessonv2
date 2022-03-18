8b385ffcc92d28016fc054c36cdc0a291b09a500
Omoefe Ugboma <Omoefe.Ugboma@investment-one.com>
2022-03-18 21:01:46 +0100

    Implemented Router Home, About and Jobs

--- vueformlessonv02/src/router/index.js
+++ vueformlessonv02/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
 
 const routes = [
   {
@@ -10,11 +13,23 @@ const routes = [
   {
     path: '/about',
     name: 'About',
+    component:About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props:true
+  },
 ]
 
 const router = createRouter({
---
+++ vueformlessonv02/src/views/jobs/Jobs.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <router-link :to="{name:'JobDetails',params:{id:job.id}}">
+        {{job.title}}
+    </router-link>
+    
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        jobs:[
+            {title: 'Web developer', id:1, details:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptates distinctio, assumenda nihil libero enim, a, nostrum aspernatur veritatis porro dolor consectetur laudantium? Accusamus minima labore esse minus, quasi necessitatibus.'},
+            {title: 'Web designer', id:2, details:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptates distinctio, assumenda nihil libero enim, a, nostrum aspernatur veritatis porro dolor consectetur laudantium? Accusamus minima labore esse minus, quasi necessitatibus.'},
+            {title: 'Data Analyst', id:3, details:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptates distinctio, assumenda nihil libero enim, a, nostrum aspernatur veritatis porro dolor consectetur laudantium? Accusamus minima labore esse minus, quasi necessitatibus.'},
+        ]
+    }
+}
+}
+</script>
+
+<style>
+
+</style>template
